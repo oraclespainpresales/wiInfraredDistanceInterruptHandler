1ca24e83ef901b03bbaced60cee5b6336f6c61b5
Carlos Casares <ccasares@gmail.com>
2018-08-16 20:38:07 +0200

    test led

---
+++ test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// Module imports
+const async = require('async')
+    , GrovePi = require('node-grovepi').GrovePi
+    , log = require('npmlog-ts')
+    , _ = require('lodash')
+;
+
+
+var urn = [ CARDM ]
+  , grovepi = new Device(GROVEPIDEV)
+  , sensors = []
+  , devices = [ grovepi ]
+  , gpsPoints = _.noop()
+  , gpsCounter = 0
+;
+
+// Initializing REST server BEGIN
+const PORT = process.env.GPSPORT || 8888
+    , restURI = '/gps'
+    , resetURI = '/resetroute'
+;
+
+// GrovePi stuff
+var board    = undefined;
+var lastData = undefined;
+var timer    = undefined;
+
+// Misc
+const PROCESS = 'PROCESS';
+const IOTCS   = 'IOTCS';
+const GROVEPI = 'GROVEPI';
+const REST    = 'REST';
+log.timestamp = true;
+
+// Detect CTRL-C
+process.on('SIGINT', function() {
+  log.info(PROCESS, "Caught interrupt signal");
+  log.info(PROCESS, "Exiting gracefully");
+  if (board) board.close()
+  board = undefined;
+  process.removeAllListeners()
+  if (typeof err != 'undefined')
+    log.error(PROCESS, err)
+  process.exit(2);
+});
+
+log.info(GROVEPI, "Initializing GrovePi devices");
+if (board)
+  callbackMainSeries(null, true);
+log.verbose(GROVEPI, 'Starting Board setup');
+board = new GrovePi.board({
+  debug: true,
+  onError: function(err) {
+    log.error(GROVEPI, 'TEST ERROR');
+    log.error(GROVEPI, err);
+  },
+  onInit: function(res) {
+    if (res) {
+      log.verbose(GROVEPI, 'GrovePi Version :: ' + board.version());
+      var led = new GrovePi.sensors.DigitalOutput(5);
+      led.turnOn();
+  }
+})
+board.init()
